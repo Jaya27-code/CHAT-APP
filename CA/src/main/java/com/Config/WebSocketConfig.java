e81596542328ff14c29150d3cde141f0fa049650
Jaya Gupta <guptajaya001@gmail.com>
2026-01-14 09:20:21 +0530

    Add files via upload

---
+++ CA/src/main/java/com/Config/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package com.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+    registry.addEndpoint("/chat")
+            .setAllowedOrigins( "http://localhost:8080")
+            .withSockJS();;
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+    registry.enableSimpleBroker( "/topic");
+    registry.setApplicationDestinationPrefixes("/app");
+    }
+}
+
+
